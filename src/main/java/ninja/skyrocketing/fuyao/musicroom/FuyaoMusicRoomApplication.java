18a84a4b0acd2761d021eeadd6b567fa3e758922
skyrocketingHong <null@skyrocketing.ninja>
2021-04-21 15:10:24 +0800

    Initial commit

---
+++ src/main/java/ninja/skyrocketing/fuyao/musicroom/FuyaoMusicRoomApplication.java
@@ -0,0 +1,13 @@
+package ninja.skyrocketing.fuyao.musicroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuyaoMusicRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuyaoMusicRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ninja/skyrocketing/fuyao/musicroom/FuyaoMusicRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package ninja.skyrocketing.fuyao.musicroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuyaoMusicRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
